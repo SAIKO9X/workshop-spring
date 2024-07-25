80d2a1a469e600e26d2b1d08c9e57f95273caa35
SAIKO9X <cardosed3@gmail.com>
2024-07-24 21:40:58 -0300

    CRUD Methods for User

---
+++ src/main/java/com/saiko/product/order/exceptions/DatabaseException.java
@@ -0,0 +1,13 @@
+package com.saiko.product.order.exceptions;
+
+import java.io.Serial;
+
+public class DatabaseException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/com/saiko/product/order/services/UserService.java
+++ src/main/java/com/saiko/product/order/services/UserService.java
@@ -1,8 +1,13 @@
 package com.saiko.product.order.services;
 
 import com.saiko.product.order.entities.User;
+import com.saiko.product.order.exceptions.DatabaseException;
+import com.saiko.product.order.exceptions.ResourceNotFoundException;
 import com.saiko.product.order.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,6 +27,36 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
         return user.get();
+    }
+
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
+
+    public void delete(Long id) {
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
+    public User update(Long id, User obj) {
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
 
